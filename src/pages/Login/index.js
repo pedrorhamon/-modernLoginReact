ba2bf707b4b9f2a7ac7d7938d97efc2bd55b15c1
Pedro <pedrorhamon16@gmail.com>
2021-05-18 20:07:48 -0300

    hiEye, hiEyeOff

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,35 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './login.css'
 
-import { MdEmail, MdLock} from "react-icons/md";
+import { MdEmail, MdLock } from "react-icons/md";
 import { BsFillPersonFill } from "react-icons/bs";
+import { HiEye, HiEyeOff } from "react-icons/hi";
 
-const Login = () => {
-    return(
+
+function Login() {
+    const [nome, setNome] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [show, setShow] = useState(false)
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        setShow(!show);
+     }
+
+    return (
         <div className="login">
             <div className="login-logo">
-                <img 
-                src="https://img.icons8.com/clouds/2x/login-rounded-right.png" alt="logo app" />
+                <img
+                    src="https://freedesignfile.com/upload/2017/08/uranus-icon-vector.png" alt="logo app" />
             </div>
             <div className="login-right">
                 <h1>Login</h1>
 
                 <div className="login-InputNome">
                     <BsFillPersonFill />
-                    <input type="text"
-                    placeholder="Digite seu nome"/>
+                    <input type="nome"
+                        placeholder="Digite seu nome"
+                        value={nome}
+                        onChange={e => setNome(e.target.value)}
+                    />
                 </div>
 
                 <div className="login-InputEmail">
                     <MdEmail />
-                    <input type="text"
-                    placeholder="Digite seu email"/>
+                    <input type="email"
+                        placeholder="Digite seu email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
                 </div>
 
                 <div className="login-InputPassword">
                     <MdLock />
-                    <input type="text"
-                    placeholder="Digite sua senha"/>
+                    <input type={show ? "text" : "password"}
+                        placeholder="Digite sua senha"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+                    <div className="login-eye">
+                        {show ? (
+                            <HiEye
+                                size={20}
+                                onClick={handleClick}
+                            />
+                        ) : (
+                            <HiEyeOff
+                                size={20}
+                                onClick={handleClick}
+                            />
+                        )}
+                    </div>
                 </div>
 
                 <button type="submit">
